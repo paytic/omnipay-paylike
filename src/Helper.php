0024301cf73f7a654eca4e642517f957f80c1b72
jay <jay@jxx.sk>
2020-11-27 13:28:53 +0100

    include currency and description in capture

--- src/Helper.php
+++ src/Helper.php
@@ -12,4 +12,13 @@ class Helper
     {
         return __DIR__ . '/resources/views/';
     }
+
+    /**
+     * @param string $descriptor
+     * @return bool
+     */
+    public static function validateDescriptor($descriptor){
+        //https://github.com/paylike/descriptor#how-is-it-validated
+        return false !== preg_match('/^[\x20-\x7E]{0,22}$/', $descriptor);
+    }
 }
